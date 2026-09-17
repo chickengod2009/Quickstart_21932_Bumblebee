a8bbab5a1fce03f4ac133366e3130edd9ed214e9
Le_Gitter <jason.camden.brown@gmail.com>
2026-09-17 11:43:10 -0500

    Update Log.java

--- TeamCode/src/main/java/org/firstinspires/ftc/Camden/Log.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Camden/Log.java
@@ -1,7 +1,8 @@
 class Log{
 	
 	protected final Opmode opmode;
-	protected File log = null;
+	protected File file = null;
+	protected ArrayList<Loggable> logs = new ArrayList<Loggable>();
 	//Maybe for better pedro access
 	protected Follower follower = null;
 	
@@ -10,11 +11,19 @@ class Log{
 		//put write with throw method here, this function is just supposed to reduce name space
 	}
 
-	public Log(Opmode ref, Follower follow){
+	public Log(Opmode ref, Follower follow) throws IOException{
 		this.opmode = ref;
 		this.file = AppUtil.getInstance().getSettingsFile("my_robot_settings.txt");
 		this.writeFile("Log: \t" + ref.toString());
-		this.follow = follow.withLogger(/*put log producing function here*/);
+		this.follow = follow.withLogger(this::getLogFunction);
+	}
+	public Log withLoggable(Loggable log){
+		this.logs.push(log);
+		return this;
+	}
+
+	public void getLogFunction(FollowerLog log){
+		//Log data here
 	}
 
 	
