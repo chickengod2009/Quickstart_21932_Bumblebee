8cb90e13a257dc9b80d26a770948fc8b1a111fd3
Le_Gitter <jason.camden.brown@gmail.com>
2026-09-24 10:07:25 -0500

    Update Field.java

--- TeamCode/src/main/java/org/firstinspires/ftc/control/Field.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/control/Field.java
@@ -6,18 +6,31 @@ public class Field {
 
 
     public static PoseBody seesaws(){
+
+        Pose topLeft = new Pose(46,90);
+        Pose topRed = new Pose(58,80);
+        Pose topBlue = new Pose(84,80);
+        Pose topRight = new Pose(95,90);
+        Pose bottomRight = new Pose(95,52);
+        Pose bottomBlue = new Pose(84,60);
+        Pose bottomRed = new Pose(58,60);
+        Pose bottomLeft = new Pose(46,52);
+
+        return new PoseBod(new Pose[]{topLeft,topRed,topBlue,topRight,bottomRight,bottomBlue,bottomRed,bottomLeft});
+        
+        
         throw  new RuntimeException("Todo!");
     }
-    public static PoseBody redGoalClose(){
+    public static Pose redGoalTop(){
         throw new RuntimeException("Todo!!");
     }
-    public static PoseBody redGoalFar(){
+    public static Pose redGoalBottom(){
         throw new RuntimeException("Todo!!");
     }
-    public static PoseBody blueGoalClose(){
+    public static Pose blueGoalTop(){
         throw new RuntimeException("Todo!!");
     }
-    public static PoseBody blueGoalFar(){
+    public static Pose blueGoalBottom(){
         throw new RuntimeException("Todo!!");
     }
 
